21d7cd59a3a60e9a0e8504e7169a1dcf693fc9d5
Olga Ovcharova <ovcharova.on@gmail.com>
2020-01-30 22:08:20 +0300

    сделала все

--- js/main.js
+++ js/main.js
@@ -22,50 +22,57 @@ var featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'condi
 var photosArr = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var checkinArr = ['12:00', '13:00', '14:00'];
 var checkoutArr = ['12:00', '13:00', '14:00'];
-
+var text = text;
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function randomElement(items) {
-  return items[Math.floor(Math.random()*items.length)];
-  }
+  return items[Math.floor(Math.random() * items.length)];
+}
 
-function uniqueElements(array) {
-  var newUniqueList = [];
-  for (var i = 0; i <=array.lenght; i++) {
-    newUniqueList.push(uniqueElements());
+
+function getRandomArr(array) {
+  var randomLenght = Math.floor(Math.random() * (array.length - 1));
+  var newArr = [];
+  for (var i = 0; i <= randomLenght; i++) {
+    newArr.push(getRandomArr());
   }
-  return newUniqueList;
+  return newArr;
 }
 
 function createObject() {
   return {
     autor: {
-      avatar: 'aimg/avatars/user' + '0' + getRandomInt(0,9) + '.png'
+      avatar: 'aimg/avatars/user' + '0' + getRandomInt(0, 9) + '.png'
     },
     offer: {
-      title: строка,
-      address:  '{{location.x}}, {{location.y}}',
+      title: text,
+      address: {
+        location: {
+          x: getRandomInt(0, 100),
+          y: getRandomInt(130, 630)
+        }
+      },
       price: getRandomInt(1000, 100000),
       type: randomElement(typeArr),
-      rooms: getRandomInt(1,10),
-      guests: getRandomInt(1,20),
-      checkin: randomElement(checkinApp),
+      rooms: getRandomInt(1, 10),
+      guests: getRandomInt(1, 20),
+      checkin: randomElement(checkinArr),
       checkout: randomElement(checkoutArr),
-      features:  массив строк случайной длины из featuresArr,
-      description: строка,
-      photos: массив строк случайной длины из photoArr
+      features: getRandomArr(featuresArr),
+      description: text,
+      photos: getRandomArr(photosArr)
     },
     location: {
-      x: что-то,
-      y: что-то
+      x: getRandomInt(0, 100),
+      y: getRandomInt(130, 630)
     }
   };
 }
 
-function createObjects()  {
-  var objectssList = [];
+function createObjects() {
+  var objectsList = [];
   for (var i = 0; i < NUMBER_OBJECTS; ++i) {
     objectsList.push(createObject());
   }
@@ -73,3 +80,24 @@ function createObjects()  {
 }
 
 var objects = createObjects();
+
+var mapDelet = document.querySelector('.map--faded');
+mapDelet.classList.remove('.map--faded');
+
+var pins = document.querySelectorAll('#pins').content.querySelector('.map__pin');
+
+var renderLocation = function () {
+  var objectElement = pins.cloneNode(true);
+  pins.style.top = objects.location.y + ;
+  pins.style.left = objects.location.x;
+  pins.style.src = objeavatar"{{author.avatar}}"
+  Альтернативный текст: alt="{{заголовок объявления}}"
+  return objectElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < objects.length; i++) {
+  fragment.appendChild(renderLocation(objects[i]));
+}
+
+pins.appendChild(fragment);
