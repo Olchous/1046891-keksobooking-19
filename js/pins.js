35216e351437f82a479672fe8a49e98f196641f9
Olga Ovcharova <ovcharova.on@gmail.com>
2020-02-22 22:16:20 +0300

    test1

---
+++ js/pins.js
@@ -0,0 +1,27 @@
+'use strict';
+var pinsArr = window.data.createPins();
+
+// поиск в разметке тега pin с классом .map__pin
+var pinsTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+// функция передачи информации из созданного objects в скопированный узел template DOM > #pin > .map__pin
+// функция возвращает отформатированный в соответствии с нашими тредованиямии template
+var renderLocation = function (pin, index) {
+  var pinElement = pinsTemplate.cloneNode(true);
+  pinElement.dataset.id = index;
+  var img = pinElement.querySelector('img');
+  pinElement.style.top = pin.location.y + 'px';
+  pinElement.style.left = pin.location.x + 'px';
+  img.setAttribute('src', pin.author.avatar);
+  img.setAttribute('title', pin.title);
+  pinElement.setAttribute('tabindex', 0);
+  return pinElement;
+};
+
+// запись полученного нового шаблона в фрагмент
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < pinsArr.length; i++) {
+  fragment.appendChild(renderLocation(pinsArr[i], i));
+}
+
+window.fragment = fragment;
